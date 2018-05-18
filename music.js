44118bcfafd8e1e67abfbec1a24bdff0fe2f6636
captainsisco <drg4176@gmail.com>
2018-05-18 13:02:56 -0300

    tabela amostra de idade, trabalho não oficial da estação

--- music.js
+++ music.js
@@ -1,12 +1,21 @@
 alert("sala de música, precione g para guitarra e b para bateria")
 var audios= document.querySelectorAll("audio")
+var gplaying= false
+var dplaying= false
 document.onkeypress=function(e){
-if(e.key==="g")
-audios[0].play()
-if(e.key==="b")
-audios[1].play()
-if(e.key==="v")
+if(e.key==="g"){
+if(gplaying)
 audios[0].pause()
-if(e.key==="c")
+else
+audios[0].play()
+gplaying= !gplaying
+}
+if(e.key==="b")
+if(dplaying)
+audios[1].play()
+else
 audios[1].pause()
+dplaying= !dplaying
+if(e.key==="q")
+speechSynthesis.speak(new SpeechSynthesisUtterance("a regra de aquisição número 147 diz: nunca gaste mais do que o necessário."))
 }
